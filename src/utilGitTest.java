ee9123897ea22a06d62ef28704732b316d6bfb51
Pavel Balakin <balakin.w2020@yandex.ru>
2025-08-23 18:39:15 +0500

    TESTGITNOTE

--- src/utilGitTest.java
+++ src/utilGitTest.java
@@ -1,5 +1,8 @@
 public class utilGitTest {
     public static void main(String[] args) {
         System.out.println("Привет, учимся пользоваться Гитом");
+        System.out.println("qq");
+
     }
+
 }
