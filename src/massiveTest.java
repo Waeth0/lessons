08403a94b9c6bce1c7dbd7b8b80a5b609f3ac46d
Pavel Balakin <balakin.w2020@yandex.ru>
2025-09-01 20:52:27 +0500

    arrayTasks

--- src/massiveTest.java
+++ src/massiveTest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class massiveTest {
     public static void main(String[] args) {
      /*  int [] array={1,3,7,4,2};
@@ -25,27 +27,46 @@ public class massiveTest {
             }
 
 
-
-
         }
         System.out.println("Task2 Индекс числа = " + indexTask2);
         System.out.println("Task2 Максимальное число массива = " + highTask2);
 
         //дан массив целых чисел, найти наименьший элемент и его индекс
 
-        int [] arrayTask3={65,32,99,105,11,3};
-        int minTask3=arrayTask3[0];
-        int indexTask3 =0;
-        for (int i = 0; i<arrayTask3.length ; i++) {
-            if (arrayTask3[i]<minTask3){
+        int[] arrayTask3 = {65, 32, 99, 105, 11, 3};
+        int minTask3 = arrayTask3[0];
+        int indexTask3 = 0;
+        for (int i = 0; i < arrayTask3.length; i++) {
+            if (arrayTask3[i] < minTask3) {
                 minTask3 = arrayTask3[i];
-                indexTask3=i;
+                indexTask3 = i;
             }
 
         }
         System.out.println("Task3 Индекс числа = " + indexTask3);
         System.out.println("Task3 Минимальное число массива = " + minTask3);
+
+        //Вводим число и проверяем есть оно в массиве или нет
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите любое число!");
+        int[] arrayTask7 = {13, 25, 12, 65, 32, 75, 13, 786};
+        int number = sc.nextInt();
+        int indexTask7 = 0;
+        boolean founded = false;
+        for (int i = 0; i < arrayTask7.length; i++) {
+
+            if (arrayTask7[i] == number) {
+                founded = true;
+                System.out.println(number);
+                return;
+            }
+
+        }
+        if (!founded) {
+            System.out.println("Такого числа нет");
+        }
+
     }
-
-
 }
+
+
