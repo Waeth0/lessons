b2ea8e866422cfd6f68f96df23879986882c8bca
Pavel Balakin <balakin.w2020@yandex.ru>
2025-09-02 15:09:10 +0500

    Task 5 and 6

--- src/massiveTest.java
+++ src/massiveTest.java
@@ -46,7 +46,7 @@ public class massiveTest {
         System.out.println("Task3 Индекс числа = " + indexTask3);
         System.out.println("Task3 Минимальное число массива = " + minTask3);
 
-        //Вводим число и проверяем есть оно в массиве или нет
+     /**   //Вводим число и проверяем есть оно в массиве или нет
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите любое число!");
         int[] arrayTask7 = {13, 25, 12, 65, 32, 75, 13, 786};
@@ -64,8 +64,32 @@ public class massiveTest {
         }
         if (!founded) {
             System.out.println("Такого числа нет");
-        }
 
+
+        }
+      */
+        //Task5 посчитать количество четных чисел в массиве
+        int [] arrayTask5={13,32,53,65,16,89,234};
+        int number5 = 0;
+        int index5 =0;
+        int count =0;
+        for (int i = 0; i <arrayTask5.length ; i++) {
+            if (arrayTask5[i]%2==0){
+                number5=arrayTask5[i];
+                index5=i;
+                count++;
+                System.out.println("Task5 Индекс числа " +index5+ " Само число " +number5);
+
+            }
+
+        }
+        System.out.println("Task5 Всего четных чисел "+count);
+
+        int [] arrayTask6={13,43,22,34,55,65};
+        for (int i = arrayTask6.length-1; i >=0 ; i--) {
+            System.out.print(arrayTask6[i]+ " ");
+
+        }
     }
 }
 
