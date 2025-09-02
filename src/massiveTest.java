140d3dc30d01d964ed01dfcb8a517e6bd760a2fe
Pavel Balakin <balakin.w2020@yandex.ru>
2025-09-02 22:39:48 +0500

    Cycles

--- src/massiveTest.java
+++ src/massiveTest.java
@@ -25,8 +25,6 @@ public class massiveTest {
             }
 
 
-
-
         }
         System.out.println("Task2 Индекс числа = " + indexTask2);
         System.out.println("Task2 Максимальное число массива = " + highTask2);
@@ -46,30 +44,30 @@ public class massiveTest {
         System.out.println("Task3 Индекс числа = " + indexTask3);
         System.out.println("Task3 Минимальное число массива = " + minTask3);
 
-     /**   //Вводим число и проверяем есть оно в массиве или нет
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите любое число!");
-        int[] arrayTask7 = {13, 25, 12, 65, 32, 75, 13, 786};
-        int number = sc.nextInt();
-        int indexTask7 = 0;
-        boolean founded = false;
-        for (int i = 0; i < arrayTask7.length; i++) {
+        /**   //Вводим число и проверяем есть оно в массиве или нет
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Введите любое число!");
+         int[] arrayTask7 = {13, 25, 12, 65, 32, 75, 13, 786};
+         int number = sc.nextInt();
+         int indexTask7 = 0;
+         boolean founded = false;
+         for (int i = 0; i < arrayTask7.length; i++) {
 
-            if (arrayTask7[i] == number) {
-                founded = true;
-                System.out.println(number);
-                return;
-            }
+         if (arrayTask7[i] == number) {
+         founded = true;
+         System.out.println(number);
+         return;
+         }
 
-        }
-        if (!founded) {
-            System.out.println("Такого числа нет");
+         }
+         if (!founded) {
+         System.out.println("Такого числа нет");
 
 
-        }
-      */
+         }
+         */
         //Task5 посчитать количество четных чисел в массиве
-        int [] arrayTask5={13,32,53,65,16,89,234};
+        int[] arrayTask5 = {13, 32, 53, 65, 16, 89, 234};
         int number5 = 0;
         int index5 = 0;
         int count5 = 0;
@@ -83,11 +81,11 @@ public class massiveTest {
             }
 
         }
-        System.out.println("Task5 Всего четных чисел "+count5);
+        System.out.println("Task5 Всего четных чисел " + count5);
         //Task6 Развернуть массив
-        int [] arrayTask6={13,43,22,34,55,65};
-        for (int i = arrayTask6.length-1; i >=0 ; i--) {
-            System.out.print(arrayTask6[i]+ " ");
+        int[] arrayTask6 = {13, 43, 22, 34, 55, 65};
+        for (int i = arrayTask6.length - 1; i >= 0; i--) {
+            System.out.print(arrayTask6[i] + " ");
 
         }
         //Task7 Каждое четное число массива умножить на 2
@@ -99,19 +97,58 @@ public class massiveTest {
             if (arrayTask7[i] % 2 == 0) {
                 number7 = arrayTask7[i];
                 increase7 = number7 * 2;
-                System.out.println("четные числа = "+number7);
-                System.out.println("Четные числа *2 = "+increase7);
+                System.out.println("четные числа = " + number7);
+                System.out.println("Четные числа *2 = " + increase7);
             }
         }
 
         //Task8 Заполнить массив только четными числами, каждый второй элемент массива умножить на 2
-        int[] arrayTask8 = {2,4,8,6,10,16,12,14};
-        int number8=0;
-        int increase8=0;
-        for (int i = 1; i <arrayTask8.length ; i+=2) {
-            arrayTask8[i]*=2;
+//        int n=10;
+//
+//        int[] arrayTask8 = new int[n];
+//        for (int i = 0; i <arrayTask8.length; i++) {
+//           arrayTask8[i]=(i+1)*2;
+//           }
+//        for (int i = 1; i <arrayTask8.length ; i+=2) {
+//            arrayTask8[i]=arrayTask8[i]*arrayTask8[i-1];
+//
+////           arrayTask8[i-1]
+//        }
+//        for (int i = 0; i <arrayTask8.length ; i++) {
+//            System.out.println(arrayTask8[i]);
+        //Task8 Заполнить массив только четными числами, каждый второй элемент массива умножить на 2
+        int n = 10;
+
+        int[] arrayTask8 = new int[n];
+        for (int i = 0; i < arrayTask8.length; i++) {
+            arrayTask8[i] = (i + 1) * 2;
+        }
+        int j = 1;
+        do {
+            arrayTask8[j]=arrayTask8[j]*arrayTask8[j-1];
+            j+=2;
+        }
+        while (j<arrayTask8.length);
+
+//        int j=1;
+//        while (j<arrayTask8.length){
+//                arrayTask8[j]=arrayTask8[j]*(arrayTask8[j-1]);
+//            j+=2;
+//        for (int i = 1; i <arrayTask8.length ; i+=2) {
+//            arrayTask8[i]=arrayTask8[i]*arrayTask8[i-1];
+
+//           arrayTask8[i-1]
+        for (int i = 0; i <arrayTask8.length ; i++) {
             System.out.println(arrayTask8[i]);
-            }
+
+        }
+        //Task8 Заполнить массив только четными числами, каждый второй элемент массива умножить на 2
+//        int number8=0;
+//        int increase8=0;
+//        for (int i = 1; i <arrayTask8.length ; i+=2) {
+//            arrayTask8[i]*=2;
+//            System.out.println(arrayTask8[i]);
+//            }
         }
     }
 
