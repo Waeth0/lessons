3e5e43d7b7926a4675922bc41f819643ec53bc8a
Pavel Balakin <balakin.w2020@yandex.ru>
2025-09-22 20:49:27 +0500

    BeginnerKitFix

--- src/novicePack/Main.java
+++ src/novicePack/Main.java
@@ -2,6 +2,9 @@ package novicePack;
 
 import java.util.ArrayList;
 
+/**
+ * Разработайте иерархию канцелярских товаров. Создайте "набор новичка", используя созданную иерархию
+ */
 public class Main {
     public static void main(String[] args) {
         NovicePack Pasha = new NovicePack();
